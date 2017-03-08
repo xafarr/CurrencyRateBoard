7bd09e8c895a9e1e923101b392260536ee708fcf
Muhammad Zafar <xafarr@gmail.com>
2017-03-08 23:19:30 +1100

    Intermediate changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {WindowRef} from "./window-ref";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  maxCurrencyRows: number = 14;
+  arr = Array(this.maxCurrencyRows).fill(1);
+
+  constructor(private windowObject: WindowRef) {
+    console.log(windowObject.nativeWindow.innerWidth);
+    console.log(windowObject.nativeWindow.innerHeight);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {WindowRef} from "./window-ref";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [WindowRef],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
